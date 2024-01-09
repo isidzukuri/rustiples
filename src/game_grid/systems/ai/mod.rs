92922a8d805a11b6ed3fa448f56fc3f4e678d1ca
isidzukuri <axesigon@gmail.com>
2024-01-09 13:44:08 +0200

    extract strategic_analysis

--- src/game_grid/systems/ai/mod.rs
+++ src/game_grid/systems/ai/mod.rs
@@ -3,11 +3,14 @@ pub mod mutation;
 pub mod pathfinding;
 pub mod pathfinding_params;
 pub mod state;
+pub mod strategic_analysis;
 
-pub use crate::game_grid::ai::mutation::*;
-use crate::game_grid::ai::pathfinding_params::*;
-use crate::game_grid::ai::state::*;
 use crate::game_grid::systems::GridEntity;
 use crate::game_grid::systems::GridEntityType;
 use crate::game_grid::traversal_cost::*;
+
+pub use mutation::*;
 pub use pathfinding::*;
+use pathfinding_params::*;
+use state::*;
+pub use strategic_analysis::*;
